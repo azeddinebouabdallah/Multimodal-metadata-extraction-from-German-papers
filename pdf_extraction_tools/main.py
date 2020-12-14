619ed438cbe6ddde7c6d90891c22e30e2a0c309c
Azeddine Bouabdallah <bazeddine@uni-koblenz.de>
2020-12-14 11:11:34 +0100

    Extract words with size and position

--- pdf_extraction_tools/main.py
+++ pdf_extraction_tools/main.py
@@ -10,12 +10,17 @@ class TextExtractor:
 
     # Works well.
     def pdfplumber_extract(self, file_location):
+        # Because documents are scanned we don't have access to the lines and rects objects of pdfplumber
         with pdfplumber.open(self.current_location+file_location) as file :
             page = file.pages[0]
-            return page.extract_text()
+            #return page.extract_text()
+
+            # Could use extract words to obtain the position and the size of each word.
+            return page.extract_words()
+            #return page.chars
 
     # Does not work with scanned documents at all.
-    def PyPDF2(self, file_location):
+    def pypdf_extract(self, file_location):
         pdfFile = PdfFileReader(self.current_location+file_location)
         page = pdfFile.getPage(0)
         return page.extractText()
@@ -30,4 +35,7 @@ if __name__ == "__main__":
 
     # Super complicated document + multi column. 
     # A problem here is that multi column is treated as single column because it is a scanned document.
+    # pypdf 0% accuracy
+    # pdfplumber 80% accurate text extraction 
+    #print(textExtractor.pypdf_extract('../ssoar_dataset/ssoar_datasetssoar-1992-sfb_186_report_nr_1.pdf'))
     print(textExtractor.pdfplumber_extract('../ssoar_dataset/ssoar_datasetssoar-1992-sfb_186_report_nr_1.pdf'))
