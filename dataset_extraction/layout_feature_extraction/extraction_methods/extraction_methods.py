b02494b33aa8ee3700208654a0dcce96b86fc0dd
Azeddine Bouabdallah <bazeddine@uni-koblenz.de>
2021-01-16 21:28:04 +0100

    Extract layout features

--- dataset_extraction/layout_feature_extraction/extraction_methods/extraction_methods.py
+++ dataset_extraction/layout_feature_extraction/extraction_methods/extraction_methods.py
@@ -1,3 +1,55 @@
 import re
-import pandas as pd
-import numpy as np
+from dateutil.parser import parse
+
+import pickle
+
+def get_word_size(y1, y2):
+    return y1 - y2
+
+def get_count_cap_letters(word):
+    if len(word) == 0:
+        return 0
+    return sum(1 for c in word if c.isupper())/len(word)
+
+def starts_cap_letter(word):
+    return 1 if word[0].isupper() else 0
+
+def get_word_length(word):
+    return len(word)
+
+def get_count_digits(word):
+    return len(re.sub("[^0-9]", "", word))
+
+def get_count_slash(word):
+    return len(re.findall('/', word))
+
+def get_count_com(word):
+    return len(re.findall(':', word))
+
+def contains_alt(word):
+    return 1 if len(re.findall('@', word)) > 0 else 0
+
+def isYear(word):
+    # From 1000 - 2999, and the word should contain only the year
+    r = re.findall(r'[1-2][0-9]{3}', word)
+    if len(r) > 0 and len(r) < 2 and len(word) == len(r[0]):
+        return 1
+    else:
+        return 0
+
+def isEmail(word):
+    r = re.findall(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', word)
+    
+    return 1 if len(r)>0 else 0
+
+def isLink(word):
+    r = re.findall(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', word)
+    return 1 if len(r)>0 else 0
+
+# Should update this to support german language
+def isDate(word):
+    try: 
+        parse(word, False)
+        return 1
+    except ValueError:
+        return 0
--- dataset_extraction/layout_feature_extraction/main.py
+++ dataset_extraction/layout_feature_extraction/main.py
@@ -1,4 +1,80 @@
 import pandas as pd
 import numpy as np
+import os
+import xml.etree.ElementTree as ET
+import pickle
+
+from extraction_methods.extraction_methods import *
+
+words_list = []
+documentName = ''
+
+
+
+file = ET.parse('test.cermstr')
+root = file.getroot()
+
+for element in root:
+    if (element.tag == 'Page'):
+        # Feature vector for the doc
+        document_vector = []
+
+        # initializing the features
+        cap_letters = 0
+        starts_cap = 0
+        line_num = 0
+        len_word = 0
+        count_num = 0
+        count_slash = 0 # /
+        count_com = 0 # :
+        is_alt = 0 # @ 
+        is_email = 0 
+        is_link = 0 
+        is_year = 0
+        is_date = 0
+        actual_word = ''
+        font_size = 0
+
+        for zone in element:
+            if zone.tag == 'Zone':
+                for line in zone:
+                    if line.tag == 'Line':
+                        for e in line:
+                            if e.tag == 'LineID': # Get the number of line.
+                                line_num = int(e.attrib['Value'])
+                            for word in e.iter('Word'):
+                                actual_word = '' # new word
+                                for we in word: # we: Word element
+                                    if we.tag == 'WordCorners': # get position
+                                        # get the top and bottom position then substract
+                                        y2 = float(we[0].attrib['y'])
+                                        y1 = float(we[2].attrib['y'])
+                                        font_size = get_word_size(y1, y2)
+                                    if we.tag == 'Character':                                        
+                                        actual_word += we[4].attrib['Value']
+                
+                                words_list.append(actual_word)
+                                cap_letters = get_count_cap_letters(actual_word)
+                                starts_cap = starts_cap_letter(actual_word)
+                                len_word = get_word_length(actual_word)
+                                count_num = get_count_digits(actual_word)
+                                count_slash = get_count_slash(actual_word)
+                                count_com = get_count_com(actual_word)
+                                is_alt = contains_alt(actual_word)
+                                is_email = isEmail(actual_word)
+                                is_link = isLink(actual_word)
+                                is_year = isYear(actual_word)
+                                is_date = isDate(actual_word)
+                                document_vector = [
+                                    cap_letters, starts_cap, line_num,
+                                    count_num, count_slash, count_com, 
+                                    is_alt, is_email, is_link, is_year, 
+                                    is_date, font_size
+                                ]
+                                
+                                with open('feature_vectors/'+actual_word.replace('/',' ')+str(len(words_list))+'.pickle', 'wb') as handle:
+                                    pickle.dump(document_vector, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
 
 
