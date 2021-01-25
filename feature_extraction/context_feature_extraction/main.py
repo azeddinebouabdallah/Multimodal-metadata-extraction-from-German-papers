0c996bc5d12d7d7485b63567a89e534308954ee7
Azeddine Bouabdallah <bazeddine@uni-koblenz.de>
2021-01-25 12:18:34 +0100

    layout + context feature concatenation

--- feature_extraction/context_feature_extraction/main.py
+++ feature_extraction/context_feature_extraction/main.py
@@ -2,6 +2,8 @@ from elmoformanylangs import Embedder
 import pickle
 from conllu import parse # consider using parse incr
 
+import pandas as pd
+
 import numpy as np
 import pandas as pd
 
@@ -24,6 +26,17 @@ class ContextFeatureExtractor:
         feature_vectors = np.array(feature_vectors)
 
         print(feature_vectors.shape)
+
+        # concatinate layout features and context 
+        layout_features_csv = pd.read_csv('../layout_feature_extraction/feature_vectors/document0vectors.csv')
+
+        layout_features = np.array(layout_features_csv)
+
+
+        feature_vectors = np.concatenate((layout_features, feature_vectors.reshape(feature_vectors.shape[0], -1)), axis=1)
+
+        df = pd.DataFrame(data=feature_vectors)
+        df.to_csv('feature_vector.csv')
 
         with open(output_location, 'wb') as handle:
             pickle.dump(feature_vectors, handle, protocol=pickle.HIGHEST_PROTOCOL)
