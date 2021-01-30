ac797234443104144758b0658c4001922b0f53b0
guiaria <prayuth.aria@gmail.com>
2021-01-31 01:26:26 +0700

    Add comment

--- pubmex/main.py
+++ pubmex/main.py
@@ -9,6 +9,11 @@ def main():
 	config_file="configs/train_config.yaml",
 	use_cuda=False,
     )
+
+    ## In case you want to get output instaed
+    ##      Change self.model.roi_heads.mask_head.register_forward_pre_hook(get_roi_heads_input) in prediction.py to self.model.roi_heads.mask_head.register_forward_hook(get_roi_heads_input), then add 1 more argument in get_roi_heads_input function
+    ##      If you want to see the entire architecture of the model, you can uncomment pdb.settrace() in __call__ function of Prediction class and print the model
+    
     _, metadata, roi_input = pubmex.predict("../ssoar_downloads/niehaus_et_al-gestandnismotivierung_in_beschuldigtenvernehmungen-ocr.pdf")
     print(roi_input)
 
