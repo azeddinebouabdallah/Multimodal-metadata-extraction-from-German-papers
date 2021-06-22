aa3cc789b0a7fcae70c3f385c19b980dae11908b
ARIA <prayuthp@uni-koblenz.de>
2021-06-22 10:49:31 +0200

    Add script to generate mexpub output

--- model/pubmex/main.py
+++ model/pubmex/main.py
@@ -1,7 +1,12 @@
-import torch
 from pubmexinference import *
+from tqdm import tqdm # Just for progress bar
+from pathlib import Path
 from torchsummary import summary
+import torch
 import pdb
+import os
+import numpy as np
+import pickle as pkl
 
 def main():
     ## Test the model
@@ -11,14 +16,32 @@ def main():
 	use_cuda=False,
     )
 
+    pdf_list = []
+    
+    for file in os.scandir('../../ssoar_dataset/'):
+        if file.name.endswith('pdf'):
+            pdf_list.append(file.name)
+
+
+    Path('vision_output2/').mkdir(parents=True, exist_ok=True)
+
+    for pdf in tqdm(pdf_list):
+        print("Current File : {0}".format(pdf))
+        if os.path.exists('vision_output2/{0}'.format(pdf).split('.')[0] + '.pickle'):
+            continue
+
+        predict = pubmex.alt_predict('../../ssoar_dataset/{0}'.format(pdf))
+
+        with open('vision_output2/{0}'.format(pdf).split('.')[0] + '.pickle', 'wb') as handle:
+            pkl.dump(predict, handle, protocol=pkl.HIGHEST_PROTOCOL)
+            handle.close()
+
+
+
+
     ## Get score for each instance
-    predict = pubmex.alt_predict("../../ssoar_downloads/niehaus_et_al-gestandnismotivierung_in_beschuldigtenvernehmungen-ocr.pdf")
-
-    ## Original pubmex
-    # v, metadata = pubmex.predict("../../ssoar_downloads/niehaus_et_al-gestandnismotivierung_in_beschuldigtenvernehmungen-ocr.pdf")
-
-    print(predict)
-    # pdb.set_trace()
+    # predict = pubmex.alt_predict("../../ssoar_downloads/niehaus_et_al-gestandnismotivierung_in_beschuldigtenvernehmungen-ocr.pdf")
+    # print(predict)
 
 
 if __name__ == '__main__':
